4c0be8b7d1ecd19b2b288b3ffc18402a5e04b9cb
terainfoai-kyh <terainfoai@gmail.com>
2026-09-03 11:52:27 +0900

    fix(live): restore missing getGooglePlaceSearchUrl import in LiveTripTab

--- src/components/LiveTripTab.jsx
+++ src/components/LiveTripTab.jsx
@@ -14,6 +14,7 @@ import {
   MapPin,
   Star
 } from 'lucide-react';
+import { getGooglePlaceSearchUrl } from '../services/geminiNlpService';
 import { CITY_LOCAL_KNOWLEDGE } from '../data/voraDialogKnowledge';
 import { getLocalizedCityName } from '../i18n/translations';
 
